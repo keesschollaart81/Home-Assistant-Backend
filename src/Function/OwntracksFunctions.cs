538d9d043f78f194841f7e633023d43110b971d3
Kees Schollaart <keesschollaart81@hotmail.com>
2018-05-14 20:50:24 +0200

    more naming

--- src/Function/OwntracksFunctions.cs
+++ src/Function/OwntracksFunctions.cs
@@ -32,31 +32,5 @@ namespace Functions
             trail.QosLevel = message.QosLevel.ToString();
             trail.Retain = message.Retain;
         }
-
-    }
-      public class Owntracks
-    {
-        public string _type { get; set; }
-        public string tid { get; set; }
-        public string acc { get; set; }
-        public string batt { get; set; }
-        public string conn { get; set; }
-        public string lat { get; set; }
-        public string lon { get; set; }
-        public string tst { get; set; }
-        public string _cp { get; set; }
-        public string alt { get; set; }
-        public string vac { get; set; }
-        public string t { get; set; }
-
-    }
-        
-    public class Trail : Owntracks
-    {
-        public string PartitionKey { get; set; }
-        public string RowKey { get; set; }
-        public string Topic { get; set; }
-        public string QosLevel { get; set; }
-        public bool Retain { get; set; }
     }
 }
