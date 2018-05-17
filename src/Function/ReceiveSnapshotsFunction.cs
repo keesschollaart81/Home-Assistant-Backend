5a11d3405ab596d17f338efe8277ca3c7e9665a6
Kees Schollaart <keesschollaart81>
2018-05-17 14:38:47 +0200

    Updating to get the CamUrl

--- src/Function/ReceiveSnapshotsFunction.cs
+++ src/Function/ReceiveSnapshotsFunction.cs
@@ -9,21 +9,43 @@ using Newtonsoft.Json;
 using CaseOnline.Azure.WebJobs.Extensions.Mqtt.Messaging;
 using CaseOnline.Azure.WebJobs.Extensions.Mqtt;
 using Microsoft.Extensions.Logging;
+using System.Text;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Net.Http.Headers;
+using System;
+using System.Configuration;
 
 namespace Functions
 {
     public static class Functions
     {
-        [FunctionName("ReceiveSnapshotsFunction")]
-        public static void ReceiveSnapshotsFunction(
-            [MqttTrigger("dafang/dafang/motion/snapshot", ConnectionString = "MqttConnectionForSnapshots")]IMqttMessage snapshop,
-            [Blob("snapshots/{sys.utcnow}.png", FileAccess.Write)] Stream outputBlob,
-             ILogger log)
+        [FunctionName("MotionFunction")]
+        public static async Task MotionFunction(
+        [MqttTrigger("dafang/dafang/motion", ConnectionString = "MqttConnectionForMotion")]IMqttMessage snapshop,
+        [Blob("snapshots/{sys.utcnow}.png", FileAccess.Write)] Stream outputBlob,
+        ILogger log)
         {
             log.LogInformation("Receiving blob");
 
+            var camUrl = ConfigurationManager.AppSettings["CamUrl"];
+            var username = ConfigurationManager.AppSettings["CamUsername"];
+            var password = ConfigurationManager.AppSettings["CamPassword"];
+
             var bytes = snapshop.GetMessage();
-            outputBlob.Write(bytes, 0, bytes.Length);
+            var on = Encoding.UTF8.GetString(bytes) == "ON";
+
+            if (on)
+            {
+                var client = new HttpClient();
+                var authHeaderValue = Convert.ToBase64String(System.Text.ASCIIEncoding.ASCII.GetBytes($"{username}:{password}"));
+                client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Basic", authHeaderValue));
+
+                var response = await client.GetAsync(camUrl);
+                var outBytes = await response.Content.ReadAsByteArrayAsync();
+
+                outputBlob.Write(outBytes, 0, outBytes.Length);
+            }
         }
     }
 }
